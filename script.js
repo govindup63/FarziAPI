49073d9c93d948cd097a1a2d7a1d18c55c5e5ac7
govind <govindup63@gmail.com>
2023-08-23 05:19:17 +0530

    file structure devided properly

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+
 function getRandomShayri() {
-    fetch("https://script.google.com/macros/s/AKfycbyOk3Z6XvFykvhkrxhQz0fSF0cpKMJQ3WOcjXii-L7K20G-VHKd1kEsv_WXUBqj8woz/exec")
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById("shayriText").textContent = data.shayri;
-        document.getElementById("shayarText").textContent = "By " + data.shayar;
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-  
-  getRandomShayri();
-  
+      fetch('https://script.google.com/macros/s/AKfycbyOk3Z6XvFykvhkrxhQz0fSF0cpKMJQ3WOcjXii-L7K20G-VHKd1kEsv_WXUBqj8woz/exec')
+        .then(response => response.json())
+        .then(data => {
+          const shayriText = document.getElementById('shayriText');
+          const shayarText = document.getElementById('shayarText');
+          shayriText.innerText = data.shayri;
+          shayarText.innerText = `- ${data.shayar}`;
+        })
+        .catch(error => {
+          const shayriText = document.getElementById('shayriText');
+          shayriText.innerText = 'Failed to fetch shayri.';
+          console.error('Error fetching shayri:', error);
+        });
+    }
+
+    getRandomShayri();
